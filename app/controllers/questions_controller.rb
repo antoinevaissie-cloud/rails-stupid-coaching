e47b01690dc5255482dfd13563406b500e135826
Antoine Vaissie <hello@antoinev.com>
2022-12-15 19:21:50 +0100

    pablo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @questions = params[:question]
+  end
+end
+
+# class PagesController < ApplicationController
+#   def contact
+#     @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+
+#     if params[:member]
+#       @members = @members.select { |member| member.start_with?(params[:member]) }
+#     end
+#   end
+# end
